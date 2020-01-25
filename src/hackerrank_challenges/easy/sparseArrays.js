37ef22a4a80160a835bbbfa828eeb4f05127be56
Meg Dahlgren <meg.dahlgren@gmail.com>
2020-01-25 12:34:10 -0500

    Add sparseArrays.js

--- src/hackerrank_challenges/easy/sparseArrays.js
+++ src/hackerrank_challenges/easy/sparseArrays.js
@@ -2,25 +2,25 @@
 query string, determine how many times it occurs in the list of input strings.
 */
 
-// const matchingStrings = (strings, queries) => {
-//   const stringCounts = {}
-//   const queryCounts = []
-//   for (const str of strings) {
-//     if (!(str in stringCounts)) {
-//       stringCounts[str] = 1
-//     }
-//     else {
-//       stringCounts[str] += 1
-//     }
-//   }
-//   for (let i = 0; i < queries.length; i++) {
-//     const query = queries[i]
-//     if (query in stringCounts) {
-//       queryCounts[i] = stringCounts[query]
-//     }
-//     else queryCounts[i] = 0
-//   }
-//   return queryCounts
-// }
+const matchingStrings = (strings, queries) => {
+  const stringCounts = {}
+  const queryCounts = []
+  for (const str of strings) {
+    if (!(str in stringCounts)) {
+      stringCounts[str] = 1
+    }
+    else {
+      stringCounts[str] += 1
+    }
+  }
+  for (let i = 0; i < queries.length; i++) {
+    const query = queries[i]
+    if (query in stringCounts) {
+      queryCounts[i] = stringCounts[query]
+    }
+    else queryCounts[i] = 0
+  }
+  return queryCounts
+}
 
 console.log('matchingStrings(): ' + JSON.stringify(matchingStrings(['def', 'de', 'fgh'], ['de', 'lmn', 'fgh'])))
